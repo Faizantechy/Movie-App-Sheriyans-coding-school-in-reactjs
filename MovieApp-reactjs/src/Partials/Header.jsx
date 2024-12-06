5c7c45af379bdbd10c90505fa226045579cda1f6
FaizanTechy <faizanraza112112@gmail.com>
2024-12-06 15:35:47 +0500

    Commit

--- MovieApp-reactjs/src/Partials/Header.jsx
+++ MovieApp-reactjs/src/Partials/Header.jsx
@@ -1,10 +1,13 @@
 import axios from '../utils/axios'
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Header = () => {
 
     const [trending, setTrending] = useState([])
   const [index, setIndex] = useState(4)
+
 
 
     const getTrendingMovies = async () => {
@@ -51,12 +54,12 @@ const Header = () => {
           })` }}>
         
      
-      <div className="img-title  w-full ">
+      <div className="img-title  w-full ml-10">
        
         
 
-        <div className=" lg:m-[5rem] m-[3rem] lg:mt-[10rem] flex flex-col justify-center items-center text-white ">
-                    <h1 className='text-3xl lg:text-4xl font-bold mb-4'>{ trending.title}</h1>
+        <div className=" lg:m-[0] m-[3rem] lg:mt-[3rem] flex flex-col  text-white justify-start ">
+                    <h1 className='text-3xl lg:text-4xl font-bold mb-4 text-left'>{ trending.title}</h1>
                     <p className="lg:max-w-[80%] w-[100%] text-lg  block">
   {trending.overview ? trending.overview.slice(0, 160) : "Loading..."} ...more
 </p>
@@ -70,18 +73,17 @@ release_date}</span></i>
 
 
             </p>
-
-            <button 
-  className="px-6 py-3 rounded-lg bg-[rgb(147,51,234)] text-white font-bold shadow-lg hover:bg-purple-800 transition-all duration-300 transform hover:scale-105 focus:outline-none z-[800] mt-4 ml-2 absolute active:border lg:flex hidden">
-  Watch Trailer
-</button>
                     
                     {/* <div className="controls space-x-5 z-[2000] w-full">
               <i className="ri-arrow-left-s-line text-[5rem] rounded-full bg-[rgb(147,51,234)] text-white hover:opacity-[0.5] active:scale-[90%]"></i>
                         <i className="ri-arrow-right-s-line text-[5rem] rounded-full bg-[rgb(147,51,234)] text-white hover:opacity-[0.5] active:scale-[90%] "></i>
 
                     </div> */}
-        </div>
+          </div>
+          {/* <Link  to={`/${trending.media_type || title}/details/${trending.id}`}
+  className="px-6 py-3 rounded-lg bg-[rgb(147,51,234)] text-white font-bold shadow-lg hover:bg-purple-800 transition-all duration-300 transform hover:scale-105 focus:outline-none z-[800]  mt-2 ml-2 mb-5 absolute active:border lg:flex ">
+  Watch Trailer
+</Link> */}
       </div>
     </div>
   )
